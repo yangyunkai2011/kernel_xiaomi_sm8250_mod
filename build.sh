b08e9bf24d6fb6635271042df1a9662ca6310c8b
yangyunkai2011 <yangyunkai2011@163.com>
2026-01-17 09:24:14 +0800

    Add files via upload

--- build.sh
+++ build.sh
@@ -95,7 +95,7 @@ echo "TARGET_DEVICE: $TARGET_DEVICE"
 
 if [ $KSU_ENABLE -eq 1 ]; then
     echo "KSU is enabled"
-    curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s ApartTUSITU
+    curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s SukiSU-Ultra
 else
     echo "KSU is disabled"
 fi
